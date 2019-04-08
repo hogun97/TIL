90559426f5842ca41ddb8e0e09ed43ee7cc5cd00
이호건 <hl928@cornell.edu>
2019-04-08 11:09:59 +0900

    Learn the new features introduced in ES7

---
+++ javascript/tutorial/ES7.js
@@ -0,0 +1,18 @@
+// New features in ES7
+
+// 1. includes()
+
+'Hello'.includes('o'); // returns true
+'Hello'.includes('x'); // returns false
+
+const pets = ['cat', 'dog', 'bat'];
+pets.includes('cat'); // returns true
+
+// 2. Exponential operator
+
+const square = (x) => x**2;
+const cube = (y) => y**3;
+
+console.log(square(5)); // returns 25
+console.log(cube(5)); // returns 125
+
