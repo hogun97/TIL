78c6eaf80a51e6bbc31b9578b3aa57001e82b23c
이호건 <hl928@cornell.edu>
2019-04-13 15:20:18 +0900

    Modify robofriends tutorial
    Delete unnecessary files and modify accordingly.

--- javascript/react/robofriends/src/index.js
+++ javascript/react/robofriends/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Card from './Card';
 import * as serviceWorker from './serviceWorker';
 
 // The following line renders what will be displayed on HTML
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<Card/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
